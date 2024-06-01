e46073c1295c53b9d9d317813879fb42f369f8ab
Farzanaltafbhat <97350316+Farzanaltafbhat@users.noreply.github.com>
2024-06-01 02:33:51 -0700

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -23,9 +23,9 @@ const Footer = () => {
             <h2>Quick Links</h2>
             <ul>
               <li><a href="/">Home</a></li>
-              <li><a href="/about">About Us</a></li>
-              <li><a href="/services">Services</a></li>
-              <li><a href="/contact">Contact</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Services</a></li>
+              <li><a href="#">Contact</a></li>
             </ul>
           </div>
           <div className="footer-section contact-form">
